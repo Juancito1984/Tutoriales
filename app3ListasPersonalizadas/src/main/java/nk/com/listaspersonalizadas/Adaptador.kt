e5082df8f341d2b65eabbbd8e20622b493a5fd5c
J Pablo <ingejpablo@gmail.com>
2017-09-25 14:33:09 -0400

    app listas personalizadas agregando imagen

--- app3ListasPersonalizadas/src/main/java/nk/com/listaspersonalizadas/Adaptador.kt
+++ app3ListasPersonalizadas/src/main/java/nk/com/listaspersonalizadas/Adaptador.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.contenedor.view.*
 
 /**
@@ -19,6 +20,7 @@ class Adaptador(context: Context, val items:MutableList<Persona>):ArrayAdapter<P
             vista=LayoutInflater.from(parent?.context).inflate(R.layout.contenedor, parent, false)
         }
 
+        ponerImagen(vista?.imagen!!, position)
         vista?.mTvNombre?.text= items[position].nombre
         vista?.mTvDeporte?.text= items[position].deporte
 
@@ -26,5 +28,17 @@ class Adaptador(context: Context, val items:MutableList<Persona>):ArrayAdapter<P
         return vista!!
 
 
+    }
+
+    fun ponerImagen(imagen:ImageView, posicion: Int){
+        when(posicion){
+            0-> imagen.setImageResource(R.drawable.hombre)
+            1-> imagen.setImageResource(R.drawable.mujer)
+            2-> imagen.setImageResource(R.drawable.hombre2)
+            3-> imagen.setImageResource(R.drawable.mujer2)
+            4-> imagen.setImageResource(R.drawable.hombre3)
+            5-> imagen.setImageResource(R.drawable.mujer3)
+            else ->imagen.setImageResource(R.mipmap.ic_launcher)
+        }
     }
 }
